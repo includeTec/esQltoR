d9a36175e6eea0555d5dea310459fd2b477d4802
Facus <faramirez87@gmail.com>
2016-07-04 10:13:36 -0300

    Controlador Acerca de y EscanearQR

---
+++ controllers/AcercaDe.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+def index():
+    titulo = "EsQltoR"
+    return dict(titulo=titulo)
+
+def aplicaciones():
+    titulo = "EsQltoR"
+    return dict(titulo=titulo)
---
+++ controllers/EscanearQR.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# try something like
+def index():
+    titulo = "EscanearQR"
+    return dict(titulo=titulo)
